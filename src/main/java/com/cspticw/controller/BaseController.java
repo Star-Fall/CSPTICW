f2e7ed28d15f3ff4ed4d47abb3e65b57b9557a99
Starfall <Starfall121039156@gmail.com>
2018-04-07 23:00:20 +0800

    log4j日志配置显示sql

--- src/main/java/com/cspticw/controller/BaseController.java
+++ src/main/java/com/cspticw/controller/BaseController.java
@@ -1,5 +1,11 @@
 package com.cspticw.controller;
 
+/**
+ * @ClassName: BaseController
+ * @author: StarFall
+ * @date: 2018年4月7日 下午10:58:14
+ * @Description:Controller基类
+ */
 public class BaseController {
 
 }
